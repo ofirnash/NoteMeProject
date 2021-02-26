f35512f19ffdb42baae2fe1d308df13827cfd6ab
Ofir Nash <ofirnash@gmail.com>
2021-02-26 23:05:27 +0200

    Added sign up page and connected

--- NoteMeApp/app/src/main/java/com/example/notemeapp/MainActivity.java
+++ NoteMeApp/app/src/main/java/com/example/notemeapp/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.notemeapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button sendToSignUpPageBtn = (Button)findViewById(R.id.btn_send_to_sign_up);
+        sendToSignUpPageBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(v.getContext(), SignUpActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
